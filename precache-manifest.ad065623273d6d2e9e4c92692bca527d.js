6dac989e2067ad9fc0aacfb6983832eb818e6697
Funmungus <Funmungus@users.noreply.github.com>
2026-05-22 22:42:09 +0000

    Deploying to build from  @ a33dfd9c4ef4409a4198788576a9b21d1958d500 🚀

---
+++ precache-manifest.ad065623273d6d2e9e4c92692bca527d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e479dbfec4e4709a60be5fadbc89d927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f14cd207515c8fb6cc",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "101d33af06f62f3408a9",
+    "url": "/static/js/2.70b8da9f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.70b8da9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f14cd207515c8fb6cc",
+    "url": "/static/js/main.904ad5c2.chunk.js"
+  },
+  {
+    "revision": "ee8c3b90c01e2a4b2e69",
+    "url": "/static/js/runtime-main.007aa73a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
