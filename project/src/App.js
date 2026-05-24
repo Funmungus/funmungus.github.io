8f70bdcb6dfe9b391e8b42344f538e2073dc3768
Jonathan Pelletier <funmungus@gmail.com>
2026-05-24 12:29:36 -0500

    Add Environment

--- project/src/App.js
+++ project/src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Experience from './Experience';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Jonathan Pelletier</h1>
         <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div className="App-content">
         <p>
           Hello. This is the self introduction page for Jonathan Pelletier. Nothing is here yet. Please look forward to it.
         </p>
         <span role="img" aria-label="bow">
           🙇‍♂️
         </span>
-      </header>
+      </div>
+      <div className="App-content-spacer"></div>
+      <Experience />
     </div>
   );
 }
 
 export default App;
+
