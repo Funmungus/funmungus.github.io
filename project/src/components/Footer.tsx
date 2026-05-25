5194071ad7e358ef52b978a8815d535298e06d19
Jonathan Pelletier <funmungus@gmail.com>
2026-05-25 17:14:50 -0500

    Add portrait

--- project/src/components/Footer.tsx
+++ project/src/components/Footer.tsx
@@ -1,27 +1,21 @@
-import React from "react";
-
-interface FooterProps {}
-
-export const Footer: React.FC<FooterProps> = ({}) => {
+function Footer() {
   return (
     <footer className="mt-auto text-white-50">
-      <p className="lead">
-        Attributes:
-        <ul className="list-group bg-dark text-white">
-          <li className="list-group-item bg-dark text-white">
-            favicon:{" "}
-            <a
-              href="https://www.flaticon.com/free-icons/spider-web"
-              title="spider web icons"
-              className="text-white"
-            >
-              Spider web icons created by Freepik - Flaticon
-            </a>
-          </li>
-        </ul>
-      </p>
+      Attributes:
+      <ul className="list-group bg-dark text-white">
+        <li className="list-group-item bg-dark text-white">
+          favicon:{" "}
+          <a
+            href="https://www.flaticon.com/free-icons/spider-web"
+            title="spider web icons"
+            className="text-white"
+          >
+            Spider web icons created by Freepik - Flaticon
+          </a>
+        </li>
+      </ul>
     </footer>
   );
-};
+}
 
 export default Footer;
